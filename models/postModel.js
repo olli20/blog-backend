eae92907f18feff279436cd996798a162e7037dc
olli20 <olena.voina@powercoders.org>
2024-11-01 20:01:58 +0100

    add gallery with multer and cloudinary

--- models/postModel.js
+++ models/postModel.js
@@ -18,7 +18,14 @@ const postModel = new Schema({
   date: {
     type: Date,
     default: Date.now,
-  }
+  },
+  gallery: [
+    {
+      source: { type: String, required: true },
+      title: { type: String, required: true },
+      alt: { type: String, required: true },
+    },
+  ],
 });
 
 const PostModel = model("post", postModel);
